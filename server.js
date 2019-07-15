8a3d41dd3af1006c6b51ecc6321f168208e2469f
Maria del carmen rodriguez apodaca <mdcra1997@outlook.com>
2019-07-15 12:13:12 -0600

    v0.0.2

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express'),
     path = require('path'),
     app = express();
 
-app.use(express.static(path.join(__dirname, 'dist', 'indqevents')));
+app.use(express.static(path.join(__dirname, '/dist/indqevents')));
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'dist', 'indqevents'));
+app.get('/*', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/dist/indqevents/index.html'));
 });
 
-app.listen(3000 || process.env.PORT, () => {
+app.listen(process.env.PORT  || 3000, () => {
     console.log('server online');
 })
